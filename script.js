1464358e97e3dd3adfe0ad30927ad108f9e5147b
Guilherme Dugaich de Oliveira <guilherme.dugaich@gmail.com>
2021-03-13 19:10:38 -0300

    Initial commit - adding projects dinamically

---
+++ script.js
@@ -0,0 +1,23 @@
+const projectSection = document.querySelector('.projects');
+
+function addProject(projectName) {
+  const projectDiv = document.createElement('div');
+  const projectImage = document.createElement('img');
+  const pageLink = document.createElement('a');
+
+  projectDiv.classList.add('project-div');
+  
+  projectImage.src = `images/${projectName}.png`;
+  projectImage.classList.add('project-image');
+
+  pageLink.innerText = projectName;
+  pageLink.href = `https://guidugaich.github.io/${projectName}`;
+  pageLink.target = '_blank'
+
+  projectDiv.appendChild(pageLink);
+  projectDiv.appendChild(projectImage);
+
+  projectSection.appendChild(projectDiv);
+}
+
+addProject('pixel-art');
